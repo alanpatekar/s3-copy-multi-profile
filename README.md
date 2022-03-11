10072de4e6a00cc82aea7a7d772ab2936f036c39
apatekar <apatekar@irobot.com>
2022-03-11 14:22:57 +0100

    - base working utility with command line arguments - first draft of README.md

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 #S3 Copy Objects using multiple boto3 sessions
 
-AWS S3 utility used to copy S3 objects from one bucket to another using two boto3 sessions.
-It also keeps all the metadata associated with the objects - 
+AWS default s3 copy API call does not work with two AWS principals (except for head object operation).
+The use case for this tool is when you have divided permissions on different AWS principals and want to
+use one principal for the get_object operation and another principal for the put_object operation in an automated fashion.
+
+Simple AWS S3 utility used to copy S3 objects from one bucket to another using two AWS profiles.
+It also keeps all the metadata associated with the objects - tags.
+
+The tool will grab the source objects and metadata, save them in memory, switch boto3 sessions and upload the 
+objects and metadata into the destination bucket.
+
+## NOTE
+
+This is a work in progress and should not be used with prod data.
+It currently works with s3 objects with only one level of "directory" prefix.
+This utility does not scale well. Tested with ~100 objects.
+
+## Usage example
+
+You can use poetry to install the environment for s3-copy-multi-profile tool.
+
+**Note**: cd into s3-copy-multi-profile directory
+
+Command to run utility:
+
+```
+python s3-copy-multi-profile.py --src_profile "my-src-profile" --src_bucket "my-src-bucket" --src_prefix "multiple" --dest_profile "my-dest-profile" --dest_bucket "my-dest-bucket" --dest_prefix "multiple"
+```
+
+## Arguments explained
+```commandline
+    :param src_s3_client: s3_client with permissions to get_object on source objects
+    :param src_bucket: source bucket
+    :param src_prefix: source s3 prefix
+    :param dest_s3_client: destination s3 client with put_object permissions
+    :param dest_bucket: destionation s3 bucket
+    :param dest_prefix: destination s3 prefix
+```
+
+## Future focus
+
+    - the base for loop does not scale well
+    - run the utility in an automated fasion, inside CodeBuild/Pipeline/Lambda
--- s3-copy-multi-profile/s3-copy-multi-profile.py
+++ s3-copy-multi-profile/s3-copy-multi-profile.py
@@ -1,32 +1,109 @@
 import click
 import logging
 import boto3
+import os
 
 
-def get_from_s3_source(s3_client, bucket_name):
-    # grabs s3 objects and metadata from source and saves it to local temp dir
-    res = s3_client.get_object(Bucket='string', Key='')
+def s3_copy(src_s3_client, src_bucket, src_prefix, dest_s3_client, dest_bucket, dest_prefix=None, logger=None):
+    """
+     Grabs src object and metadata, saves them locally and copies the objects and metadata into destination bucket.
+    :param src_s3_client: s3_client with permissions to get_object on source objects
+    :param src_bucket: source bucket
+    :param src_prefix: source s3 prefix
+    :param dest_s3_client: destination s3 client with put_object permissions
+    :param dest_bucket: destionation s3 bucket
+    :param dest_prefix: destination s3 prefix
+    :return:
+    """
+    logger.info("S3-copy-multi-profile START")
+    objects = src_s3_client.list_objects_v2(Bucket=src_bucket, Prefix=src_prefix)['Contents']
 
-    print(res)
+    for obj in objects:
+        # get key, get tags, download locally,  switch session, upload object, put tag, remove local object
+
+        # remove prefix from src object
+        removed_prefix = obj['Key'].split("/")[-1]
+        # download object locally
+        res_download = src_s3_client.download_file(src_bucket, obj['Key'], removed_prefix)
+
+        # upload object in destination
+        dest_key = f"{dest_prefix}/{removed_prefix}"
+        local_file = open(removed_prefix, 'rb')
+        res_put_obj = dest_s3_client.put_object(Body=local_file,Bucket=dest_bucket, Key=dest_key)
+
+        # get object tags from src object
+        tag_res = src_s3_client.get_object_tagging(Bucket=src_bucket, Key=obj.get('Key'))
+
+        # put src object tags on dest object
+        put_tags_response = dest_s3_client.put_object_tagging(
+            Bucket=dest_bucket,
+            Key=dest_key,
+            Tagging={'TagSet': tag_res.get('TagSet')})
+
+        logger.debug(f'Successfully copied {removed_prefix} into {dest_bucket}.')
+
+        # cleanup downloaded local file
+        os.remove(removed_prefix)
+
+    logger.info("S3-copy-multi-profile DONE.")
+
+
+def handle_arguments(kwargs, logger=None):
+    """
+    Handle s3-copy-multi-profile arguments, establish boto3 sessions and s3 clients.
+    :param kwargs:
+    :param logger:
+    :return: dict {"src_s3_client":src_s3_client,
+           "src_bucket": src_bucket,
+           "src_prefix": src_prefix,
+           "dest_s3_client": dest_s3_client,
+           "dest_bucket": dest_bucket,
+           "dest_prefix": dest_prefix,
+           "logger": logger,
+           }
+    """
+    src_profile = kwargs.get('src_profile')
+    src_session = boto3.session.Session(profile_name=src_profile)
+    src_bucket = kwargs.get('src_bucket')
+    src_prefix = kwargs.get('src_prefix')
+    src_s3_client = src_session.client('s3')
+
+    dest_profile = kwargs.get('dest_profile')
+    dest_session = boto3.session.Session(profile_name=dest_profile)
+    dest_bucket = kwargs.get('dest_bucket')
+    dest_prefix = kwargs.get('dest_prefix')
+    dest_s3_client = dest_session.client('s3')
+
+    res = {"src_s3_client":src_s3_client,
+           "src_bucket": src_bucket,
+           "src_prefix": src_prefix,
+           "dest_s3_client": dest_s3_client,
+           "dest_bucket": dest_bucket,
+           "dest_prefix": dest_prefix,
+           "logger": logger,
+           }
+
+    return res
 
 
 @click.command()
-#@click.option('--envname', help="Override envname from account")
+@click.option('--src_profile', help="Override envname from account")
+@click.option('--src_bucket', help="Override envname from account")
+@click.option('--src_prefix', help="Override envname from account")
+@click.option('--dest_profile', help="Override envname from account")
+@click.option('--dest_bucket', help="Override envname from account")
+@click.option('--dest_prefix', help="Override envname from account")
 @click.help_option()
-def run(** cmdargs):
+def _run(** cmdargs):
     """
-    Delete all objects in path
+    Run s3-copy-multi-profile utilty.
     """
     logger = logging.getLogger()
 
-    # cmdargs = dsc.handle_common_cli_args(cmdargs, has_aws=True, logger=logger)
-    # session = cmdargs.get('session')
-    #
-    # res = delete_s3_objects(session)
-    source_profile = 'aws-it-sec-sbx-03.Developer'
-    session = boto3.session.Session(profile_name=source_profile)
+    cmdargs = handle_arguments(cmdargs, logger=logger)
+    s3_copy(**cmdargs)
 
 
 if __name__ == '__main__':
 
-    run()
+    _run()
